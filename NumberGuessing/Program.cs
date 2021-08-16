2cd679d33f7c6ba59df8b95adc7cbf6d73f13ec0
aminarehman14 <46981456+aminarehman14@users.noreply.github.com>
2021-08-16 16:41:52 -0500

    updating with abdullahs code
    update to include if statements

--- NumberGuessing/Program.cs
+++ NumberGuessing/Program.cs
@@ -15,21 +15,83 @@ namespace NumberGuessing
             int RightNum = R.Next(0, 100);
 
             bool correct = false;
-
-            do {
-                Console.Write("Enter a number between 1 to 100 "); 
+            bool parseSuccess = false;
+            do
+            {
+                Console.Write("Enter a number between 1 to 100 ");
                 //todo accept user input ("read user input from console c#"
-                string i = Console.ReadLine();
-                Console.WriteLine(i); 
-                int x = int.Parse(i);
+                string guessedNum = Console.ReadLine();
+
+                //exception for no input
+                while (string.IsNullOrEmpty(guessedNum))
+                {
+                    Console.WriteLine("Ain't nobody got time fo dat, guess a number >:|");
+                    guessedNum = Console.ReadLine();
+                }
+
+                //exception for invalid input
+                while (parseSuccess == false)
+                {
+                    parseSuccess = int.TryParse(guessedNum, out int guess);
+
+                    if (parseSuccess == false)
+                    {
+                        Console.WriteLine("Just a number loser");
+                        guessedNum = Console.ReadLine();
+                    }
+                } 
+
+
+
                 // TODO write statement that checks if a number is less than or higher than the RIGHTNUM and write too high 
                 //if the entered num is too high and too low if the answer is too low
                 // if the answer is right print that is correct and set win = true
+                /*
+                switch (x)
+                {
+                    case x == RightNum:
+                        Console.WriteLine("shaBAAM You right.");
+                        correct = true;
+                        break;
+                    case x > RightNum:
+                        Console.WriteLine("Too high.  Try again");
+                        break;
+                    case x < RightNum:
+                        Console.WriteLine("Too low. Try again");
+                        break;
+                    default:
+                        Console.WriteLine("Nothing to say.");
+                        break;
+                }
+                */
+                int x = int.Parse(guessedNum);
+
+                if (x == RightNum)
+                {
+                    Console.WriteLine("shaBAAM You right.");
+                    correct = true;
+                }
+                else if (x > RightNum)
+                {
+                    Console.WriteLine("Too high.  Try again");
+                }
+                else if (x < RightNum)
+                {
+                    Console.WriteLine("Too low. Try again");
+                }
+                else
+                {
+                    Console.WriteLine("Nothing to say.");
+                }
+
 
 
             } while (correct == false); 
 
-            Console.ReadLine(); 
+            Console.WriteLine("Games over, go home.");
+            Console.WriteLine("Press Enter to exit");
+
+            string choice = Console.ReadLine();
         }
     }
 }
