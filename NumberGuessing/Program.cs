6708ab065db98cf5122a4dd65c2d344a5359779e
aminarehman14 <46981456+aminarehman14@users.noreply.github.com>
2021-08-16 14:56:36 -0500

    push readme file
    push readmefile

--- NumberGuessing/Program.cs
+++ NumberGuessing/Program.cs
@@ -10,6 +10,26 @@ namespace NumberGuessing
     {
         static void Main(string[] args)
         {
+            Random R = new Random();
+
+            int RightNum = R.Next(0, 100);
+
+            bool correct = false;
+
+            do {
+                Console.Write("Enter a number between 1 to 100 "); 
+                //todo accept user input ("read user input from console c#"
+                string i = Console.ReadLine();
+
+                int x = int.Parse(i);
+                // TODO write statement that checks if a number is less than or higher than the RIGHTNUM and write too high 
+                //if the entered num is too high and too low if the answer is too low
+                // if the answer is right print that is correct and set win = true
+
+
+            } while (correct == false); 
+
+            Console.ReadLine(); 
         }
     }
 }
---
+++ README.txt
@@ -0,0 +1,3 @@
+https://www.codecademy.com/learn/learn-c-sharp/modules/learn-csharp-logic-conditionals/cheatsheet
+
+https://www.codecademy.com/learn/learn-c-sharp Inheritance/ Classes and objects
